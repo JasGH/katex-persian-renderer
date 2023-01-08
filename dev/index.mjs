0240ea32044181b37deb837b8c18cd37af4ec7fc
JasGH <yasaman.gonehi@gmail.com>
2023-01-08 16:00:16 +0330

    node js support

--- dev/index.mjs
+++ dev/index.mjs
@@ -1,10 +1,10 @@
 import katex from 'katex'
 import 'katex/dist/katex.css'
-import persianKatexPlugin from 'src/index.mjs'
+import addPersianTo from 'src/index.mjs'
 import 'katex-persian-fonts/index.css'
 import allMetrics from 'katex-persian-fonts/fontMetrics.json'
 
-persianKatexPlugin(katex, {
+addPersianTo(katex, {
     fontName: 'iranyekan',
     fontMetrics: allMetrics
 })
